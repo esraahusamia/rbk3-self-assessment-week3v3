7380cbc636584a5eb01aae3903a3212eaf67172e
esraahusamia <esraa.hus93@yahoo.com>
2017-09-09 09:41:38 -1200

    easyBackbone

--- backbone-pond/fishModel.js
+++ backbone-pond/fishModel.js
@@ -2,11 +2,27 @@
 
 var Fish = Backbone.Model.extend({
 
-  defaults: {
-    name: 'Larry',
-    image: 'http://www.google.com',
-    description: 'Regular old fish',
-    displayInfo: false
-  }
+	defaults: {
+		name: 'Larry',
+		image: 'http://www.google.com',
+		description: 'Regular old fish',
+		displayInfo: false
+	},
+
+
+	initialize: function() {
+    this.render();
+    this.model.on('toggle:description', this.render, this);
+  },
+
+	// render:function(){
+
+	// }
+
+
+	toggleDescription:function (){
+
+		this.model.on("click", this.render,this)
+	}
 
 });
